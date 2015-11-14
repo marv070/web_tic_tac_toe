0512f804ca7be70f11d5e6979c2fabfbd07c08cf
Marvin Laucher <marv070@gmail.com>
2015-11-14 13:19:29 -0500

    functionall code for web tic tac toe

--- online_version.rb
+++ online_version.rb
@@ -7,17 +7,25 @@ game = TTTgame.new(@board, "", "", 1)
 
 
 get '/tictactoe' do
-	erb :Tictactoe_board1, :locals => {:board => game.board,:message => "Let's play some tic tac toe!", :message2 => "Wanna Pick X or O ?", :message3 => ""}
+	erb :Tictactoe_board1, :locals => {:board => game.board,
+									   :message => "Let's play some tic tac toe!", 
+									   :message2 => "Please Pick X or O ?", 
+									   :message3 => "", 
+									   :message4 => ""}
 
 end
 
 
 post '/tictactoe' do
   game.player1 = params[:selection].upcase
-  
+  player_marker = game.current_player()
+	
 	if game.marker_valid?(game.player1) == true
 		game.player2 = game.p2(game.player1)
-		erb :Tictactoe_board2, :locals => {:current => game.current, :message => "Player 1 is #{game.player1} and Player 2 is #{game.player2}.", :board => game.board}
+		erb :Tictactoe_board2, :locals => {:current => game.current, 
+										   :message => "Player 1 is #{game.player1} and Player 2 is #{game.player2}.",
+										   :message5 => "Pick a Square Player #{player_marker} ", 
+										   :board => game.board}
 	end
 end
 
@@ -30,15 +38,24 @@ post '/board' do
 			
 			if game.is_board_full?(game.board) == false && game.win(game.board) == false
 				game.current = game.switch_players()
-				erb :Tictactoe_board2, :locals => { :current => game.current, :message => "Player #{player_marker} has chosen square #{params[:choice]}.", :board => game.board }
+				erb :Tictactoe_board2, :locals => { :current => game.current, 
+					                                :message => "Player #{player_marker} has chosen square #{params[:choice]}.", 
+					                                :message5 => "Pick a Square Player #{game.current}",
+					                                :board => game.board }
+			
 			elsif game.win(game.board) == true
-				erb :gamewon, :locals => {:message => "Player #{player_marker} has won.", :board => game.board }
+				erb :gamewon, :locals => {:message => "Player #{player_marker} has won.", 
+										  :board => game.board }
+			
 			else game.is_board_full?(game.board) == true
-			erb :gamewon, :locals => {:message => "Players have tied.", :board => game.board }
+			erb :gamewon, :locals => {:message => "Players have tied.", 
+				                      :board => game.board }
 			end
 				
 	else
-		erb :Tictactoe_board2, :locals => { :current => game.current, :message => "Square taken, Please choose again.", :board => game.board }
+		erb :Tictactoe_board2, :locals => { :current => game.current, 
+			                                :message => "Square taken, Please choose again.", 
+			                                :board => game.board }
 	end
  end
 
@@ -49,7 +66,10 @@ post '/replay' do
 			redirect to('/tictactoe')
 			
 		else
-			erb :Tictactoe_board1, :locals => {:board => game.board,:message => "To bad your gonna play over and over and over", :message2 => "!!!Pick X or O!!!", :message3 => "It will Never End!!!! HAHAHA"}
+			erb :Tictactoe_board1, :locals => {:board => game.board,
+											   :message => "Lets play again", 
+											   :message2 => "!!!Pick X or O!!!", 
+											   :message3 => "You must PLAY AGAIN OR ELSE"}
 	
 		end
 
